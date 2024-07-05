fd8d8c928bd9af78adc201a70d47f1bd24556b35
artem <shurtem@mail.ru>
2024-07-05 16:30:01 +0300

    ДЗ 1 сделано.

---
+++ script.js
@@ -0,0 +1,14 @@
+function hw1_1() {
+	console.clear();
+	let degreesCelsius = 100;
+	let degreesFahrenheit = (9 / 5) * degreesCelsius + 32;
+	console.log(`Температура в градусах Цельсия: ${degreesCelsius}`);
+	console.log(`Температура в градусах Фаренгейта: ${degreesFahrenheit}`);
+}
+
+function hw1_2() {
+	console.clear();
+	const myName = "Artem";
+	let admin = myName;
+	console.log(`admin: ${admin}`);
+}
